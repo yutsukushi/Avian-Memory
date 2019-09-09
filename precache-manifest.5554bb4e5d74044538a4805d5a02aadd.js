730cc7cdaff5c978f9fedbe4d3fa8fd7c319d08b
yutsukushi <ykushibi@gmail.com>
2019-09-09 19:47:22 -0400

    Updates

---
+++ precache-manifest.5554bb4e5d74044538a4805d5a02aadd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4416be0539949f6b797ef70eac5eb3fe",
+    "url": "/Clicky-Game/index.html"
+  },
+  {
+    "revision": "400d1dcd9378642c886e",
+    "url": "/Clicky-Game/static/css/main.87608dbc.chunk.css"
+  },
+  {
+    "revision": "997c1737f3d3c7a8e3bf",
+    "url": "/Clicky-Game/static/js/2.c73320c9.chunk.js"
+  },
+  {
+    "revision": "400d1dcd9378642c886e",
+    "url": "/Clicky-Game/static/js/main.dced2b24.chunk.js"
+  },
+  {
+    "revision": "55ed8aacfd3926e788ea",
+    "url": "/Clicky-Game/static/js/runtime~main.1209a1e5.js"
+  }
+]);
